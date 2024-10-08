c99236b102d703a6d57c61f60adc86bd02590cda
Elpodval <161817706+Elpodval@users.noreply.github.com>
2024-10-08 19:37:40 +0300

    Add files via upload

---
+++ Module_9_6.py
@@ -0,0 +1,12 @@
+def all_variants(text):
+    length = len(text)
+
+    # Генерируем все подпоследовательности
+    for start in range(length):
+        for end in range(start + 1, length + 1):
+            yield text[start:end]  # Возвращаем подпоследовательность
+
+
+a = all_variants("Hello")
+for i in a:
+    print(i)
