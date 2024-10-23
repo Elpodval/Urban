3412582ac31d5b16a8d79bbb8045b900e8239c00
Elpodval <161817706+Elpodval@users.noreply.github.com>
2024-10-23 17:56:03 +0300

    Add files via upload

--- module_10_3_a.py
+++ module_10_3_a.py
@@ -10,24 +10,22 @@ class Bank:
     def deposit(self):
         for i in range(100):
             amount = random.randint(50, 500)  # Случайное число для пополнения
-            with self.lock:  # Блокировка потока
-                self.balance += amount
-                if self.balance >= 500 and self.lock.locked():
-                    self.lock.release()  # Разблокировка, если баланс >= 500
-                print(f"Пополнение: {amount}. Баланс: {self.balance}")
+            self.balance += amount
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()  # Разблокировка, если баланс >= 500
+            print(f"Пополнение: {amount}. Баланс: {self.balance}")
             time.sleep(0.001)  # Ожидание
 
     def take(self):
         for i in range(100):
             amount = random.randint(50, 500)  # Случайное число для снятия
             print(f"Запрос на {amount}")
-            with self.lock:
-                if amount <= self.balance:
-                    self.balance -= amount
-                    print(f"Снятие: {amount}. Баланс: {self.balance}")
-                else:
-                    print("Запрос отклонён, недостаточно средств")
-                    self.lock.acquire()  # Блокировка потока, если недостаточно средств
+            if amount <= self.balance:
+                self.balance -= amount
+                print(f"Снятие: {amount}. Баланс: {self.balance}")
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                self.lock.acquire()  # Блокировка потока, если недостаточно средств
             time.sleep(0.001)  # Ожидание
 
 # Создание объекта класса Bank
